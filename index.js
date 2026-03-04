cf08606087ce72bdd0dc1638f8c23e9d46357c31
JP-003 <jpcooper2005@gmail.com>
2026-03-04 10:17:11 +0900

    Added js consts and api key

---
+++ index.js
@@ -0,0 +1,4 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "8e2d81f77bf781eee278d91811303875";
