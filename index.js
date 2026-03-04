0bab1e09f837c6b3d66e152524829c508808a3a8
jcooper-6 <jcooper6@ufl.edu>
2026-03-04 12:27:31 +0900

    Implemented the getWeatherData function including a call to the api

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ weatherForm.addEventListener("submit", async event => {
 
 async function getWeatherData(city){
 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+
+    console.log(response);
+
+    if (!response.ok) {
+        throw new Error("Could not fetch weather data");
+    }
+    return await response.json();
 }
 
 function displayWeatherInfo(data){
